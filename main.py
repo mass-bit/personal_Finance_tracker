4034a96763c66100f1222f74901f7b7aaa96ec95
mass-bt <masuk27roll@gmail.com>
2024-10-18 11:44:42 +0530

    create a class for creating and initialize the csv

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+# importing all require module
 import pandas as pd 
 import csv
 from datetime import datetime
+
+# Creating a Class "CSV" for creating csv file and initialization
+
+class CSV:
+    CSV_file= "finance_data.csv"
+    
+    @classmethod
+    def initialize_csv(cls):
+        try:
+            pd.read_csv(cls.CSV_file)
+        except FileNotFoundError:
+            df= pd.DataFrame(columns=["date", "amoun", "category", "description"])
+            df.to_csv(cls.CSV_file, index=False)
+
+CSV.initialize_csv()
